d4d889f7920b55b6cb371af74712a78f87bcc470
Angelo <angelomiguelcasia@gmail.com>
2026-04-27 12:59:35 +0800

    added some changes in app.js

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const path = require('path');
+require('dotenv').config();
+
 const app = express();
 
+// Set up EJS for your Views
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Middleware
+app.use(express.static(path.join(__dirname, 'public'))); // For CSS, Images, JS
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(session({
+    secret: 'vaniday_secret_key',
+    resave: false,
+    saveUninitialized: true
+}));
+
+// Basic Test Route
 app.get('/', (req, res) => {
-  res.send('Server running');
+    // This is where you will eventually render your 13 icons
+    res.send('Vaniday Project is Running!');
 });
 
-app.listen(3000, () => {
-  console.log('http://localhost:3000');
+// Start Server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
