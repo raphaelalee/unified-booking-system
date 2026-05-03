425cddebbb0c4f338839cb751fd9baa4e7ea4454
Angelo <angelomiguelcasia@gmail.com>
2026-05-03 22:57:08 +0800

    changed booking confirmation page

--- app.js
+++ app.js
@@ -53,6 +53,7 @@ app.get('/promotions/one-for-one', allowGuestOrCustomer, (req, res) => {
 app.get('/promotions/featured-salons', allowGuestOrCustomer, merchantController.showFeaturedSalons);
 app.get('/merchants', allowGuestOrCustomer, merchantController.listMerchants);
 app.get('/profile', userController.showProfile);
+app.get('/my-services', requireCustomer, userController.showMyServices);
 app.get('/reward-shop', requireCustomer, userController.showRewardShop);
 app.post('/reward-shop/claim', requireCustomer, userController.claimRewardShopDaily);
 app.get('/membership', requireCustomer, (req, res) => {
